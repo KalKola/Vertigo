2c9222d3b76b02a4923f8d7fce8f1488f36905dc
Kalyan Kola Cahill <kalyankolacahill@gmail.com>
2018-03-01 16:35:37 +0000

    Respawn Points and Spikes

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentSpawn;
+
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RespawnPlayer()
+    {
+        Debug.Log("Player Respawn");
+        player.transform.position = currentSpawn.transform.position;
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            levelManager.currentSpawn = gameObject;
+            Debug.Log("Activated Checkpoint");
+        }
+    }
+}
