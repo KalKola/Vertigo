65dc2c80da06fbdc9d5b59b72087bff5ede1e7c3
hadinisamudeen <d16123244@mydit.ie>
2018-04-25 10:06:17 +0100

    65486548654865486548654865486548$

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+    public int pointsToAdd;
+    public AudioSource coinsfx;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<PlayerAnimController>() == null)
+        {
+            return;
+        }
+        ScoreManager.AddPoints(pointsToAdd);
+
+        Destroy(gameObject);
+        coinsfx.Play();
+    }
+}
